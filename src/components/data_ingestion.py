cab640e2eec2f5f84b79484f4f7fba5d628dbf75
Parth Dobariya <parthdobariya62@gmail.com>
2023-12-19 18:38:10 +0530

    Data transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import pandas as pd
-from src.logger import logging
-from src.exception import CustomException
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.logger import logging
+from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig():
@@ -43,4 +45,8 @@ class DataIngestion():
 
 if __name__ =='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    
